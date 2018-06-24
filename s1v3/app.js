60bff19272c8cd678869300931cd8f49a1542b4b
Mark Sweet <marks828@gmail.com>
2018-06-24 13:07:57 -0400

    upload commit

---
+++ s1v3/app.js
@@ -0,0 +1,20 @@
+const helloWorld = new Vue({
+  el: '#hellovue',
+  data: {
+    title: 'hello world',
+    message: 'This is my first Vue Template'
+  }
+});
+
+const example = new Vue({
+  el: '#example',
+  data: {
+    title: 'hello ',
+    message: 'This is your daily cat-firmation',
+    name: 'mark',
+    img: {
+      src: "https://placeimg.com/200/200/animals",
+      alt: "A placeholder image of animals",
+    }
+  }
+});
