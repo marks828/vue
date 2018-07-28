1f5ddb9203cc017988eabcae2472b5ca9237b091
Mark Sweet <marks828@gmail.com>
2018-07-28 15:57:53 -0400

    new folder and updated

--- other/js-vue-accordion/s3v5/book_detail/app.js
+++ other/js-vue-accordion/s3v5/book_detail/app.js
@@ -4,11 +4,6 @@ const book = new Vue({
       title: 'The Sirens of Titan',
       author: 'Kurt Vonnegut',
       summary: 'This is a summary of the Sirens of Titan.',
-      showDetail: false,
-    },
-    methods:{
-      toggleDetails: function(){
-        this.showDetail = !this.showDetail
-      },
-    },
+      showDetail: false
+    }
 });
